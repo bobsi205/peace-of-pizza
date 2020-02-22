7861651b1477cdd16f539a18f279f3cb6bb9c467
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-02-22 19:49:54 +0200

    route to app bootstrap inported navbar & layout

---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                <h2>peace of pizza</h2>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
