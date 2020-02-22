fcd5d7ddd10ccc412fc5ba63c3f73c10dc840646
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-02-22 20:00:45 +0200

    fixed navbar buttons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 class Layout extends Component {
   render() {
-
     return (
       <div>
         <Navbar />
         <div className="container">{this.props.children}</div>
+        <Footer />
       </div>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
-import { Nav, Form, FormControl, Button, Navbar } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 
 export class navbar extends Component {
   render() {
     return (
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="/">Peace Of Pizza</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/order">Order Now</Nav.Link>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
+        <Nav>
+          <Nav.Link href="/login">Login</Nav.Link>
+        </Nav>
       </Navbar>
     );
   }
