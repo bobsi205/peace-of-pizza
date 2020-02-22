edfc7a1b8367b56da181dfe0800172953c9c3166
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-02-22 23:48:15 +0200

    added login page and routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import Layout from "./components/Layout";
 import LandingPage from "./components/LandingPage";
 import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
 
 class App extends Component {
   render() {
     const route = (
       <Switch>
         <Route exact path={"/"} component={() => <LandingPage />} />
-        {/* <Route path={"/edituser/:id"} component={() => <EditUser user />} /> */}
+        <Route path={"/login"} component={() => <Login />} />
       </Switch>
     );
 
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Login extends Component {
+  render() {
+    return (
+      <div className="d-flex justify-content-center align-items-center">
+        asd
+      </div>
+    );
+  }
+}
+
+export default Login;
