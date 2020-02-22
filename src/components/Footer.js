bd9f72dcba311201d1b6d35bb3b2a889e53b563e
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-02-22 20:00:25 +0200

    stock footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Jumbotron, Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <Jumbotron fluid>
+      <Container>
+        <p>Peace Of Pizza</p>
+        <small>
+          Our pizza unique pizza is made from the sweat of shalom out great
+          founder
+        </small>
+      </Container>
+    </Jumbotron>
+  );
+};
+
+export default Footer;
