b9e3931b44233e9b7c834e737ec6a900853a2915
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-02-27 10:05:51 +0200

    split register to a diffrent page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from "./components/Layout";
 import LandingPage from "./components/LandingPage";
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/login/Login";
+import Register from "./components/login/Register";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <Switch>
         <Route exact path={"/"} component={() => <LandingPage />} />
         <Route path={"/login"} component={() => <Login />} />
+        <Route path={"/register"} component={() => <Register />} />
       </Switch>
     );
 
