17d4db3792eabdebc000a58a4e85fa888eb11fa8
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-03-05 00:09:31 +0200

    pizza backround with temp img. pizza loading after first click

--- src/components/PizzaBuilder.js
+++ src/components/PizzaBuilder.js
@@ -1,22 +1,35 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import Paparoni from "../images/toppings/paparoni.png";
-
+import Paparoni from "../images/toppings/temp.png";
+import Pizza from "../images/Pizza2.0.png";
 const PizzaBuilder = props => {
-  const [locations, setLocations] = React.useState([]);
+  //{rx,ry,topping,sx}
+  const [Toppings, setToppings] = React.useState([]);
   const canvasRef = React.useRef(null);
-  const peperoniRef = React.useRef(null);
   var imageObj1 = new Image();
+  var pizzaImg = new Image();
   imageObj1.src = Paparoni;
+  pizzaImg.src = Pizza;
 
   React.useEffect(() => {
     const canvas = canvasRef.current;
     const loc = canvas.getBoundingClientRect();
     const ctx = canvas.getContext("2d");
-    console.log(locations, canvas.getBoundingClientRect());
+    console.log(Toppings, canvas.getBoundingClientRect());
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    locations.forEach(location =>
-      ctx.drawImage(imageObj1, location.x - loc.left, location.y - loc.top)
+    ctx.drawImage(pizzaImg, 0, 0, canvas.width, canvas.height);
+    Toppings.forEach(location =>
+      ctx.drawImage(
+        imageObj1,
+        location.part,
+        0,
+        50,
+        50,
+        location.x - loc.left,
+        location.y - loc.top,
+        50,
+        50
+      )
     );
   });
 
@@ -34,12 +47,17 @@ const PizzaBuilder = props => {
           <Button> peperoni </Button>
         </div>
         <canvas
+          style={{ backgroundImage: "url(../images/Pizza2.0.png)" }}
           ref={canvasRef}
           width={500}
           height={500}
           onClick={e => {
-            const newLocation = { x: e.clientX, y: e.clientY };
-            setLocations([...locations, newLocation]);
+            const newLocation = {
+              x: e.clientX - 25,
+              y: e.clientY - 25,
+              part: Math.floor(Math.random() * Math.floor(5)) * 50
+            };
+            setToppings([...Toppings, newLocation]);
           }}
         />
       </div>
