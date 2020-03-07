2964834bcb3e10073de90d207283b47ec05c46e8
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-03-07 20:22:42 +0200

    imported all images into toppings variable

--- src/components/PizaaCanvas.js
+++ src/components/PizaaCanvas.js
@@ -1,22 +1,67 @@
 import React from "react";
 import imgPepperoni from "../images/toppings/pepperoni.png";
 import imgArtichoke from "../images/toppings/artichoke.png";
-// import imgBabyMozzarella from "../images/toppings/BabyMozzarella.png";
+import imgBabyMozzarella from "../images/toppings/babyMozzarella.png";
+import imgBeef from "../images/toppings/beef.png";
+import imgBellPepers from "../images/toppings/bellPepers.png";
+import imgBOlives from "../images/toppings/bOlives.png";
+import imgBroccoli from "../images/toppings/broccoli.png";
+import imgBulgarianCheese from "../images/toppings/bulgarianCheese.png";
+import imgCabanos from "../images/toppings/cabanos.png";
+import imgCorn from "../images/toppings/corn.png";
+import imgExtraCheese from "../images/toppings/extraCheese.png";
+import imgGarlic from "../images/toppings/garlic.png";
+import imgGOlives from "../images/toppings/gOlives.png";
+import imgJalapeno from "../images/toppings/jalapeno.png";
+import imgMushrooms from "../images/toppings/mushrooms.png";
+import imgSheepCheese from "../images/toppings/sheepCheese.png";
+import imgTomato from "../images/toppings/tomato.png";
+
 import Pizza from "../images/Pizza2.0.png";
 
 const PizaaCanvas = () => {
   //{rx,ry,topping,sx}
   const [ToppingsLoc, setToppingsLoc] = React.useState([]);
   const canvasRef = React.useRef(null);
-  var imageObj1 = new Image();
-  var imageObj2 = new Image();
   var pizzaImg = new Image();
-  imageObj1.src = imgPepperoni;
   var toppings = {
-    pepperoni: imageObj1
+    pepperoni: new Image(),
+    artichoke: new Image(),
+    BabyMozzarella: new Image(),
+    beef: new Image(),
+    bellPepers: new Image(),
+    bOlives: new Image(),
+    broccoli: new Image(),
+    bulgarianCheese: new Image(),
+    cabanos: new Image(),
+    corn: new Image(),
+    extraCheese: new Image(),
+    garlic: new Image(),
+    gOlives: new Image(),
+    jalapeno: new Image(),
+    mushrooms: new Image(),
+    sheepCheese: new Image(),
+    tomato: new Image()
   };
+  toppings.pepperoni.src = imgPepperoni;
+  toppings.artichoke.src = imgArtichoke;
+  toppings.BabyMozzarella.src = imgBabyMozzarella;
+  toppings.beef.src = imgBeef;
+  toppings.bellPepers.src = imgBellPepers;
+  toppings.bOlives.src = imgBOlives;
+  toppings.broccoli.src = imgBroccoli;
+  toppings.bulgarianCheese.src = imgBulgarianCheese;
+  toppings.cabanos.src = imgCabanos;
+  toppings.corn.src = imgCorn;
+  toppings.extraCheese.src = imgExtraCheese;
+  toppings.garlic.src = imgGarlic;
+  toppings.gOlives.src = imgGOlives;
+  toppings.jalapeno.src = imgJalapeno;
+  toppings.mushrooms.src = imgMushrooms;
+  toppings.sheepCheese.src = imgSheepCheese;
+  toppings.tomato.src = imgTomato;
+
   pizzaImg.src = Pizza;
-  toppings = { ...toppings, artichoke: imageObj1 };
 
   React.useEffect(() => {
     const canvas = canvasRef.current;
@@ -29,7 +74,7 @@ const PizaaCanvas = () => {
     ctx.drawImage(pizzaImg, 0, 0, canvas.width, canvas.height);
     ToppingsLoc.forEach(location =>
       ctx.drawImage(
-        toppings.pepperoni,
+        toppings.jalapeno,
         location.part,
         0,
         50,
@@ -46,8 +91,8 @@ const PizaaCanvas = () => {
       <canvas
         style={{ backgroundImage: "url(../images/Pizza2.0.png)" }}
         ref={canvasRef}
-        width={500}
-        height={500}
+        width={400}
+        height={400}
         onClick={e => {
           const canvas = canvasRef.current;
           const loc = canvas.getBoundingClientRect();
