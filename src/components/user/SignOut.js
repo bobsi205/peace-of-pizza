cae732725060913ff30148d4f0977c54a7b451bd
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-03-18 23:43:05 +0200

    added signout logic need to fix context not updating

--- src/components/user/SignOut.js
+++ src/components/user/SignOut.js
@@ -1,12 +1,24 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useContext, useEffect } from "react";
+import { CartContext } from "../../context/CartContext";
 
-const SignOut = () => {
+import { withRouter } from "react-router-dom";
+const SignOut = props => {
+  const [getCart, setCart] = useContext(CartContext);
+
+  useEffect(() => {
+    let tempCart = getCart;
+    tempCart.loggedIn = false;
+    tempCart.currentUser = {};
+    setCart(tempCart);
+    console.log(getCart);
+    props.history.push("/");
+  });
+
   return (
-    <Container>
+    <div>
       <h1 className="display-4">Well shit you can't logout.</h1>
-    </Container>
+    </div>
   );
 };
 
-export default SignOut;
+export default withRouter(SignOut);
