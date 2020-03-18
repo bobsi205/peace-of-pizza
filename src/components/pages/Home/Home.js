ac9f7be4db56dad2b94b30163a78778a2f44d718
attiasbarak <attias.barak@gmail.com>
2020-03-18 11:24:46 +0200

    +

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Form, Button, InputGroup } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 import Navigation from "../../common/Navigation/Navigation";
 import Reviews from "./Review/Reviews";
