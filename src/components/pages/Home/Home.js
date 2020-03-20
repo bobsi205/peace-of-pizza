3a4d01b1a35a5e0903916db8e68defb22ab499c0
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-03-20 22:12:18 +0200

    feed me btn fixed with logic

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container, Button } from "react-bootstrap";
 import { withRouter } from "react-router-dom";
 import Navigation from "../../common/Navigation/Navigation";
 import Reviews from "./Review/Reviews";
 import OrderGuide from "./OrderGuide/OrderGuide";
 import Social from "./Social/Social";
+import { CartContext } from "../../../context/CartContext";
 
 import imgBanner from "./images/banner-pizza-dim.jpg";
 
 const Home = props => {
+  const { getCart } = useContext(CartContext);
   const styleHeader = {
     backgroundImage: `url(${imgBanner})`,
     backgroundSize: "cover",
@@ -18,7 +20,7 @@ const Home = props => {
 
   return (
     <>
-      <header className="bg-light" style={styleHeader}>
+      <header className="bg-light d-flex flex-column" style={styleHeader}>
         <Navigation noBackground />
         <Container className="text-center py-4">
           <img
@@ -32,8 +34,12 @@ const Home = props => {
           variant="primary"
           size="lg"
           type="submit"
-          className="text-nowrap m-1"
-          onClick={() => props.history.push("/order/stage-1")}
+          className="text-nowrap m-1 align-self-center"
+          style={{ width: "30%" }}
+          onClick={() => {
+            if (getCart.currentUser === "") props.history.push("/sign-in");
+            else props.history.push("/order/stage-1");
+          }}
         >
           Feed Me!
         </Button>
